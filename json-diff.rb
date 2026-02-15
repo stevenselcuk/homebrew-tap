b545b470bdb2bed622252ab8fe380ed5b8876cf4
Steven J. Selcuk <stevenjselcuk@gmail.com>
2026-02-15 13:15:01 +0200

    Update json-diff.rb
    URL has been changed

--- json-diff.rb
+++ json-diff.rb
@@ -1,7 +1,7 @@
 class JsonDiff < Formula
   desc "Steven's json diff"
   homepage "https://github.com/stevenselcuk/json-diff"
-  url "https://github.com/stevenselcuk/json-diff/releases/download/v0.1.0/json-diff-macos.tar.gz"
+  url "https://github.com/stevenselcuk/json-diff/releases/download/v0.1.0/json-diff-aarch64-apple-darwin.tar.gz"
   sha256 "sha256:fbf370444729e20409f57c90355ca65683c21c0a898ceb1bafe7fe41aabab450"
   version "0.1.0"
 
