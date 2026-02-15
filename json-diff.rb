8bdff558d63d341b5737b51ac67a58daa6a5e6f0
Steven J. Selcuk <stevenjselcuk@gmail.com>
2026-02-15 13:12:52 +0200

    Create json-diff.rb

---
+++ json-diff.rb
@@ -0,0 +1,11 @@
+class JsonDiff < Formula
+  desc "Steven's json diff"
+  homepage "https://github.com/stevenselcuk/json-diff"
+  url "https://github.com/stevenselcuk/json-diff/releases/download/v0.1.0/json-diff-macos.tar.gz"
+  sha256 "sha256:fbf370444729e20409f57c90355ca65683c21c0a898ceb1bafe7fe41aabab450"
+  version "0.1.0"
+
+  def install
+    bin.install "json-diff"
+  end
+end
