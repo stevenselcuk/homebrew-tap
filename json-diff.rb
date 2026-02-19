a18685883a4a075a9c84a4bb3cba92703658b100
Steven J. Selcuk <stevenjselcuk@gmail.com>
2026-02-19 18:58:33 +0200

    Update json-diff.rb

--- json-diff.rb
+++ json-diff.rb
@@ -1,9 +1,9 @@
 class JsonDiff < Formula
   desc "Steven's json diff"
   homepage "https://github.com/stevenselcuk/json-diff"
-  url "https://github.com/stevenselcuk/json-diff/releases/download/v0.1.0/json-diff-aarch64-apple-darwin.tar.gz"
-  sha256 "fbf370444729e20409f57c90355ca65683c21c0a898ceb1bafe7fe41aabab450"
-  version "0.1.0"
+  url "https://github.com/stevenselcuk/json-diff/releases/download/v0.2.2/json-diff-aarch64-apple-darwin.tar.gz"
+  sha256 "d18433498790af11059acaa8546d2b6aa8f04575f61b6a6917e205fda32ce01f"
+  version "0.2.2"
 
   def install
     bin.install "json-diff"
